c98b9165400fc6410f5eb6c0251eafbaf0b8aac0
ADDv72 <146644845+ADDv72@users.noreply.github.com>
2023-11-05 23:16:16 +0700

    Add files via upload

--- BukuPerpustakaan.java
+++ BukuPerpustakaan.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class BukuPerpustakaan {
     
@@ -8,19 +9,19 @@ public class BukuPerpustakaan {
     String kategori;
     String tahun;
 
-    BukuPerpustakaan( String judul, String author, String penerbit, String kategori, String tahun){
+    BukuPerpustakaan(String judul, String author, String penerbit, String kategori, String tahun){
         this.judul = judul;
         this.author = author;
         this.penerbit = penerbit;
         this.kategori = kategori;
         this.tahun = tahun;
     }
+
     public static void main(String[] args){
 
-         System.out.println("=========================================================================================");
-         System.out.println(" Daftar Buku yang Tersedia ");
-         System.out.println("=========================================================================================");
- 
+        System.out.println("=========================================================================================");
+        System.out.println(" Daftar Buku yang Tersedia ");
+        System.out.println("=========================================================================================");
 
         ArrayList<BukuPerpustakaan> daftarPerpustakaan = new ArrayList<BukuPerpustakaan>();
         daftarPerpustakaan.add(new BukuPerpustakaan(" Laskar Pelangi    ", " Andrea Hirata          ", " Bentang Pustaka        ", " Fiksi      ", " 2005   "));
@@ -28,27 +29,38 @@ public class BukuPerpustakaan {
         daftarPerpustakaan.add(new BukuPerpustakaan(" Ayat Ayat Cinta   ", " Habiburrahman El-Shiray", " Republika              ", " Romansa    ", " 2004   "));
         daftarPerpustakaan.add(new BukuPerpustakaan(" Perahu kertas     ", " Dee Lestari            ", " Bentang Pustaka        ", " Romansa    ", " 2008   "));
         daftarPerpustakaan.add(new BukuPerpustakaan(" Tentang Kamu      ", " Tere Liye              ", " Gramedia Pustaka Utama ", " Romansa    ", " 2007   "));
+
         System.out.println(" Judul             "+ " Author                 " + " Penerbit               " + " Kategori   " + " Tahun "); 
         System.out.println("========================================================================================="); 
 
         for(BukuPerpustakaan item : daftarPerpustakaan){
-            
             System.out.println(item.judul + item.author + item.penerbit + item.kategori + item.tahun); 
         }
+
         int bukuBaru = 0;
         int bukuLawas = 0;
-        if (bukuBaru > 2000) {
-            bukuLawas++;
-        } else {
-            bukuBaru++;
+
+        // Assuming you want to check the publication year of each book
+        for (BukuPerpustakaan item : daftarPerpustakaan) {
+            int year = Integer.parseInt(item.tahun.trim());
+            if (year > 2000) {
+                bukuBaru++;
+            } else {
+                bukuLawas++;
+            }
         }
+
         System.out.println("=========================================================================================");
         System.out.println("Jumlah buku yang tersedia : "+daftarPerpustakaan.size());
-
         System.out.println("Buku Lawas: " + bukuLawas);
         System.out.println("Buku Baru: " + bukuBaru);
 
+        // Adding Stack functionality
+        Stack<BukuPerpustakaan> stackBuku = new Stack<>();
+        for(BukuPerpustakaan item : daftarPerpustakaan){
+            stackBuku.push(item);
+        }
 
-      
+        System.out.println("Stack buku: " + stackBuku);
     }
 }
